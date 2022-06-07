cd325fa3559160a3f1f3b8ce623748986bcddf6a
vladnest2003 <vladnest2003@gmail.com>
2022-06-07 16:00:39 +0300

    Added New Repo

---
+++ EFCTesting/Interfaces/ITestUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using EFCTesting.DataModels;
+using EFCTesting.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCTesting.Interfaces
+{
+    public interface ITestUserRepository : IRepository<TestUser> 
+    {
+
+    }
+}
---
+++ EFCTesting/Repository/TestUserRepository.cs
@@ -0,0 +1,46 @@
+﻿using EFCTesting.DataModels;
+using EFCTesting.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCTesting.Repository
+{
+    public class TestUserRepository : ITestUserRepository
+    {
+        private readonly EnglishContext _context;
+        public TestUserRepository(EnglishContext context)
+        {
+            _context = context;
+        }
+        public Task Add(TestUser entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TestUser entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TestUser>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TestUser> GetByid(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TestUser question)
+        {
+            var result = _context.TestUser.FirstOrDefault(x => x.UserId == question.UserId && x.TestId == question.TestId);
+            result.Progress = question.Progress;
+            result.Done = question.Done;
+            _context.SaveChanges();
+        }
+    }
+}
